8f11fe0d965e6fc09b094cbefddbcbb7df3ba4a6
basmasalim <basmasalim030@gmail.com>
2025-07-14 22:51:10 +0300

    feat: add ingredient section component with HTML, SCSS, and TypeScript files

---
+++ src/app/features/components/home-page/ingredient-section/ingredient-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingredient-section',
+  imports: [],
+  templateUrl: './ingredient-section.html',
+  styleUrl: './ingredient-section.scss'
+})
+export class IngredientSection {
+
+}
