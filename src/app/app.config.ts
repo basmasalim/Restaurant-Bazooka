bbdb9c6849e86228048544bf709ea2643c72f2d1
basmasalim <basmasalim030@gmail.com>
2025-07-14 20:55:26 +0300

    feat: add Firebase integration with Firestore support
    - Added @angular/fire package to enable Firebase services.
    - Updated app.config.ts to initialize Firebase and Firestore.
    - Created environment.ts to store Firebase configuration details.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,18 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    // Firebase Initialization
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore())
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyClcIINqB2U7qoN6vfce49Pdq-oNYzqWXQ",
+    authDomain: "bazookaegy-7151b.firebaseapp.com",
+    projectId: "bazookaegy-7151b",
+    storageBucket: "bazookaegy-7151b.firebasestorage.app",
+    messagingSenderId: "673081315035",
+    appId: "1:673081315035:web:cc74780588a0af6ea506fe",
+    measurementId: "G-J5P2GW4KNV"
+  }
+};
