9b5b8d1f1820ed950969345a2948f5109de13d3b
basmasalim <basmasalim030@gmail.com>
2025-07-15 16:22:23 +0300

    feat: implement hero section and delivery section components with corresponding styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
+import { Home } from "./features/pages/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Home],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/features/components/home-page/delivery-section/delivery-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery-section',
+  imports: [],
+  templateUrl: './delivery-section.html',
+  styleUrl: './delivery-section.scss'
+})
+export class DeliverySection {
+
+}
