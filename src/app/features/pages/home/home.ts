efa5f9ad3abd6c6fad43851c3e872a84713456c9
basmasalim <basmasalim030@gmail.com>
2025-07-20 20:49:29 +0300

    feat(offers): add offers component with basic structure and styling

--- src/app/features/pages/home/home.ts
+++ src/app/features/pages/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroSection } from '../../components/home-page/hero-section/hero-section';
 import { SpecialOffers } from '../../components/home-page/special-offers/special-offers';
+import { MenuPreview } from '../../components/home-page/menu-preview/menu-preview';
+import { Offers } from '../../components/offers/offers';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroSection, SpecialOffers],
+  imports: [HeroSection, SpecialOffers, MenuPreview, Offers],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
